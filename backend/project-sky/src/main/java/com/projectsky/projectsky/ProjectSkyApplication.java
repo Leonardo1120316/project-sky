27a1cde85b3e0428333cf47a76e7d6cbb9ad041f
leo <w18968850835@163.com>
2024-06-13 23:56:17 +0800

    项目前后端初始化

---
+++ backend/project-sky/src/main/java/com/projectsky/projectsky/ProjectSkyApplication.java
@@ -0,0 +1,13 @@
+package com.projectsky.projectsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSkyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSkyApplication.class, args);
+    }
+
+}
